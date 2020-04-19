39ed36c8f29aca810434e3abf92c189d57d013f9
Ryan <ryancheta@users.noreply.github.com>
2020-04-19 16:35:10 -0700

    fixed state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import React from "react";
 import { hot } from "react-hot-loader";
+import React, { useState } from "react";
+import "./App.scss";
 
 function App() {
+  const [message, setMessage] = useState("San Jose");
+  const cities = [
+    "San Jose",
+    "Los Angeles",
+    "San Diego",
+    "San Francisco",
+    "Seattle",
+    "New York",
+    "Miami",
+    "Austin",
+    "Dallas",
+    "Portland"
+  ];
+
+  let getRandomInt = max => {
+    return Math.floor(Math.random() * Math.floor(max));
+  };
+
+  let handleClick = () => {
+    setMessage(cities[getRandomInt(10)]);
+    console.log("WHAT THE FUCK?!?!");
+  };
+
   return (
     <div className="App">
-      <h1>Hello React From Scratch!</h1>
+      <button onClick={() => handleClick()}>change message</button>
+      <h1 id="title">Hello React From {message}</h1>
     </div>
   );
 }
