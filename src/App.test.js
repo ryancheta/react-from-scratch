313d1ffb8f6414c107a5bcc2ea15638264a2ff44
ryancheta <8141274+ryancheta@users.noreply.github.com>
2021-10-19 01:33:47 -0700

    integrate renderer

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "@testing-library/jest-dom";
 import { render, screen, cleanup } from "@testing-library/react";
+import renderer from "react-test-renderer";
 import App from "./App";
 
 test("test", () => {
   render(<App />);
   const app = screen.getByTestId("app");
   expect(app).toBeInTheDocument();
+});
+
+test("snapshot test", () => {
+  const tree = renderer.create(<App />).toJSON();
+  console.log(tree.children);
 });
