eac32c4999dc93ffbba97de9bae4d415a9224093
ryancheta <8141274+ryancheta@users.noreply.github.com>
2021-12-31 08:41:19 -0800

    dispatch response to redux

--- src/components/sideNav/index.js
+++ src/components/sideNav/index.js
@@ -1,9 +1,12 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useEffect } from "react";
 import { classes } from "utils";
 import NavItem from "./NavItem";
 import styles from "./styles.mod.scss";
 import { Main } from "App";
 import CultureAssessment from "../CultureAssessment";
+import { setInsights } from "../../store/state/insights/actions";
+import useFetch from "../hooks/useFetch";
+import { useDispatch } from "react-redux";
 
 const routes = [
   { path: "/", exact: true, main: <Main /> },
@@ -11,6 +14,14 @@ const routes = [
 ];
 
 const SideNav = forwardRef(({ navItems, className, ...rest }, ref) => {
+  const dispatch = useDispatch();
+  const url =
+    "http://ec2-18-223-122-214.us-east-2.compute.amazonaws.com:8000/apis/v1/account6/insights";
+  const { response, error, loading } = useFetch(url, null);
+
+  useEffect(() => {
+    dispatch(setInsights(response?.data));
+  }, [response]);
   return (
     <nav ref={ref} className={classes(className, styles.nav)} {...rest}>
       <ul>
@@ -19,16 +30,8 @@ const SideNav = forwardRef(({ navItems, className, ...rest }, ref) => {
             <NavItem key={item.id} itemName={item.name} itemIcon={item.icon} />
           ))}
       </ul>
-      {/* <Switch>
-        <Route path={`/`} component={Main} />
-        <Route path={`/culture-assessment`} component={CultureAssessment} />
-      </Switch> */}
     </nav>
   );
 });
 
 export default SideNav;
-
-// {routes.map((route, idx) => (
-//   <Route key={idx} path={route.path} component={<route.main />} />
-// ))}
