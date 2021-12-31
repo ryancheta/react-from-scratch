1cccdd63bfed8a8b6c2f4a0d53fffa8970291c07
ryancheta <8141274+ryancheta@users.noreply.github.com>
2021-12-31 08:39:59 -0800

    get goals from redux global state

--- src/components/goals/index.js
+++ src/components/goals/index.js
@@ -2,19 +2,11 @@ import React from "react";
 import { classes } from "utils";
 import { flex, centerItems } from "assets/scss/_flex.mod.scss";
 import Goal from "./Goal";
-
-const goals = [
-  { id: 1, title: "increase women leaders", number: "22,400", percent: "50%" },
-  { id: 2, title: "increase black leaders", number: "16,220", percent: "42%" },
-  {
-    id: 3,
-    title: "increase black employees",
-    number: "14,238",
-    percent: "40%",
-  },
-];
+import { useSelector } from "react-redux";
 
 const Goals = (props) => {
+  const { goals } = useSelector((state) => state.insights.insights);
+
   return (
     <div style={{ marginBottom: "1rem" }}>
       <div className={classes(flex, centerItems)}>
@@ -33,12 +25,12 @@ const Goals = (props) => {
         </button>
       </div>
       <div className={flex} {...props}>
-        {goals.map((goal) => (
+        {goals?.map(({ title, count, percent }) => (
           <Goal
-            key={goal.id}
-            title={goal.title}
-            number={goal.number}
-            percent={goal.percent}
+            key={percent + title}
+            title={title}
+            count={count}
+            percent={percent}
           />
         ))}
         <h4>+ Add Goals</h4>
